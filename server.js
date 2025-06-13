508b81b82e8f480acf4218c91c4f8f8e61a95879
devtaitv <taikaka2015@gmail.com>
2025-06-13 16:52:41 +0700

    update cors

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'https://diem10na.vercel.app',
+  credentials: true
+}));
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
